0ee2adbd0be9fcdbc02328deca710144535b01b8
svx9a <grecoaek@yahoo.co.th>
2025-12-30 18:23:35 +0700

    Delete Desktop directory

--- Desktop/AE-REAL-ESTATE/agency-ai-whitelabel/dashboard-vue/src/utils/ramp.ts
+++
@@ -1,7 +0,0 @@
-export default function(scheme: any[]) {
-  return function(t: number) {
-    var n = scheme.length;
-    var i = Math.max(0, Math.min(n - 1, Math.floor(t * n)));
-    return scheme[i];
-  };
-}
